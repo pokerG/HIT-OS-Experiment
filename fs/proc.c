30b0da442d5db97ad36958b7971bdcfbb86443e0
pokerG <pokerfacehlg@gmail.com>
2014-11-30 17:37:16 +0800

    add flag

--- fs/proc.c
+++ fs/proc.c
@@ -27,17 +27,20 @@ int proc_read(int idev,int dev,char * buf,int count,off_t * pos){
 		char* offpsbuffer = psbuffer;
 		struct task_struct * p;
 		int offset = 0;
+		if(psflag){
 			offset = sprintf(offpsbuffer,"%s\t%s\t%s\t%s\t%s\n","pid","state","father","counter","start_time");
 			offpsbuffer += offset;
-			for(i = 0;i <= NR_TASKS;i++){
+			for(i = 0;i < NR_TASKS;i++){
 				p = task[i];
-				if(p == NULL) break;
+				if(p == NULL) continue;
 				offset = sprintf(offpsbuffer,"%d\t%d\t%d\t%d\t%ld\n",p->pid,p->state,p->father,p->counter,p->start_time);
 				offpsbuffer += offset;
 			}
+			psflag = 0;
+		}
 		int total = 0;
 		for(i = 0; i < count; i++){
-			if(psbuffer[i + *pos] == '\0') break;
+			if(psbuffer[i + *pos] == '\0') {psflag = 1; break;}
 			total ++;
 			put_fs_byte(psbuffer[i + *pos],&buf[i + *pos]);
 		}
@@ -56,49 +59,59 @@ int proc_read(int idev,int dev,char * buf,int count,off_t * pos){
 		int used_inodes = 0;
 		int free_inodes = 0;
 		int fsb = 1; //逻辑位图与节点位图第一位都不用
+		int sum = 0;
 		for(i = 0; i < sb->s_zmap_blocks;i++){
 			bh = sb->s_zmap[i];
 			for(j = 0; j < 1024; j++){
 				for(k = 0; k < 8;k++){
+					if(sum == total_blocks) goto end1;
 					if(fsb) fsb = 0;
 					else{
-						if((1<<k & bh->b_data[j]) == 0)
-							free_blocks++;
+						if((1<<k & bh->b_data[j]))
+							used_blocks++;
+							sum++;
 					}
 				}
 			}
 		}
+		end1:
 		fsb = 1;
+		sum = 0;
 		for(i = 0; i < sb->s_imap_blocks;i++){
 			bh = sb->s_imap[i];
 			for(j = 0; j < 1024; j++){
 				for(k = 0; k < 8;k++){
+					if(sum == total_inodes) goto end2;
 					if(fsb) fsb = 0;
 					else{
-						if((1<<k & bh->b_data[j]) == 0)
-							free_inodes++;
+						if((1<<k & bh->b_data[j]))
+							used_inodes++;
+							sum++;
 					}
 				}
 			}
 		}
-		used_blocks = total_blocks - free_blocks;
-		used_inodes = total_inodes - free_inodes;
-		offset = sprintf(offhdbuffer,"%s%d%s\n","total_blocks:",total_blocks,";");
-		offhdbuffer += offset;
-		offset = sprintf(offhdbuffer,"%s%d%s\n","free_blocks:",free_blocks,";");
-		offhdbuffer += offset;
-		offset = sprintf(offhdbuffer,"%s%d%s\n","used_blocks:",used_blocks,";");
-		offhdbuffer += offset;
-   		offset = sprintf(offhdbuffer,"%s%d%s\n","total_inodes:",total_inodes,";");
-		offhdbuffer += offset;
-		offset = sprintf(offhdbuffer,"%s%d%s\n","free_inodes:",free_inodes,";");
-		offhdbuffer += offset;
-		offset = sprintf(offhdbuffer,"%s%d%s\n","used_inodes:",used_inodes,";");
-		offhdbuffer += offset;
-
+		end2:
+		free_blocks = total_blocks - used_blocks;
+		free_inodes = total_inodes - used_inodes;
+		if(hdflag){
+			offset = sprintf(offhdbuffer,"%s%d%s\n","total_blocks:",total_blocks,";");
+			offhdbuffer += offset;
+			offset = sprintf(offhdbuffer,"%s%d%s\n","free_blocks:",free_blocks,";");
+			offhdbuffer += offset;
+			offset = sprintf(offhdbuffer,"%s%d%s\n","used_blocks:",used_blocks,";");
+			offhdbuffer += offset;
+   			offset = sprintf(offhdbuffer,"%s%d%s\n","total_inodes:",total_inodes,";");
+			offhdbuffer += offset;
+			offset = sprintf(offhdbuffer,"%s%d%s\n","free_inodes:",free_inodes,";");
+			offhdbuffer += offset;
+			offset = sprintf(offhdbuffer,"%s%d%s\n","used_inodes:",used_inodes,";");
+			offhdbuffer += offset;
+			hdflag = 0;
+		}
 		int total = 0;
    		for(i = 0; i < count; i++){
-			if(hdbuffer[i + *pos] == '\0') break;  
+			if(hdbuffer[i + *pos] == '\0') {hdflag = 1; break;}  
 			total ++;
 			put_fs_byte(hdbuffer[i + *pos],&buf[i + *pos]);
 		}
@@ -117,12 +130,15 @@ int proc_read(int idev,int dev,char * buf,int count,off_t * pos){
 		struct m_inode * inr; 
 		int pinodenum = 100;
 		*pos = 0;
-		if(inode_hasprint != pinodenum){
-			inode_hasprint ++;
-			inr = iget(sb->s_dev,inode_hasprint);
-			offset = sprintf(offinbuffer,"%s%d%s%d\n","inr:",inode_hasprint,";zone[0]:",inr->i_zone[0]);
-			offinbuffer += offset;
-			iput(inr);		
+		if(inflag){
+			if(inode_hasprint != pinodenum){
+				inode_hasprint ++;
+				inr = iget(sb->s_dev,inode_hasprint);
+				offset = sprintf(offinbuffer,"%s%d%s%d\n","inr:",inode_hasprint,";zone[0]:",inr->i_zone[0]);
+				offinbuffer += offset;
+				iput(inr);		
+			}
+			inflag = 0;
 		}
 		int total = 0;
 		for(i = 0; i < count; i++){
@@ -131,6 +147,7 @@ int proc_read(int idev,int dev,char * buf,int count,off_t * pos){
 					inode_hasprint = 0;
 					if(total) reset_inode = 1;
 				}
+				inflag = 1;
 				break;
 			}else{
 				total ++;
